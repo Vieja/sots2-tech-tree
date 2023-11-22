a415b437dd955937188628fb8c8264b2bc37cff5
Vieja <mwiechcz@gmail.com>
2023-11-22 23:40:42 +0100

    added margin on left

--- js/settings.js
+++ js/settings.js
@@ -27,7 +27,7 @@ var techTree = (function(api) {
     nodeInnerHeight: 64,
     spriteSheetWidth: 512,
     spriteSheetHeight: 512,
-    margin: { top: 20, right: 0, bottom: 20, left: 0 }
+    margin: { top: 20, right: 0, bottom: 20, left: 20 }
   };
   api.durations = {
     activateLink: 750,
